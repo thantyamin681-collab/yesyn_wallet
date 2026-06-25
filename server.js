246f810849edfd5a10bdc0a9baf9308014b3b95f
LEON <thantyamin681@gmail.com>
2026-06-25 11:34:43 +0630

    Add files via upload

--- server.js
+++ server.js
@@ -42,12 +42,12 @@ const upload = multer({ storage: storage });
 
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }
+ host: process.env.MYSQLHOST,      // မျဉ်းကြောင်းချိတ်ရင် ဒီနာမည်သုံးရပါတယ်
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
